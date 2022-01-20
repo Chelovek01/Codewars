090426bbbe8c0f0d4232b51a8e1b1278c7ab1944
BaranBayan <85448845+BaranBayan@users.noreply.github.com>
2022-01-20 16:25:03 +0300

    1

---
+++ Js/Square Every Digit.js
@@ -0,0 +1,10 @@
+function squareDigits(num){
+  num = num.toString();
+  let result ="";
+  for (let i=0; i<num.length; i++)
+  {
+  result += Math.pow(num.charAt(i),2)+"";
+  }
+  return parseInt(result);
+
+}
