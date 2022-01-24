13c75a9031f2b975339024a20e36a57610a62f37
BaranBayan <85448845+BaranBayan@users.noreply.github.com>
2022-01-24 09:06:28 +0300

    tasks

---
+++ Js/String ends with.js
@@ -0,0 +1,10 @@
+//--------------------------------------------------Task----------------------------------------------------------------
+// Complete the solution so that it returns true if the first argument(string)
+// passed in ends with the 2nd argument (also a string).
+///------------------------------------------------Solution-------------------------------------------------------------
+
+function solution(str, ending){
+  if(str.endsWith(ending) == true){
+    return true;
+  }else return false;
+};
